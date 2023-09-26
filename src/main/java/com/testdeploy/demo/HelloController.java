590ecc8e3ef9e39c780dfff05d4bfb6f3c4c6094
jongwanra <najongsi@gmail.com>
2023-09-26 17:44:58 +0900

    test jenkins 14

--- src/main/java/com/testdeploy/demo/HelloController.java
+++ src/main/java/com/testdeploy/demo/HelloController.java
@@ -9,6 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 public class HelloController {
 	@GetMapping()
 	public ResponseEntity helloWorld(){
-		return ResponseEntity.from("hello-world!13");
+		return ResponseEntity.from("hello-world!14");
 	}
 }
