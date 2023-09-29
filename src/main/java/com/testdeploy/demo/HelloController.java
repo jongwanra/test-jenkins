63af2f192218ea6917a54057ba3d01f60d7b0266
jongwanra <najongsi@gmail.com>
2023-09-29 13:15:16 +0900

    test jenkins 22

--- src/main/java/com/testdeploy/demo/HelloController.java
+++ src/main/java/com/testdeploy/demo/HelloController.java
@@ -9,6 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 public class HelloController {
 	@GetMapping()
 	public ResponseWrapper helloWorld(){
-		return ResponseWrapper.from("hello-world!21");
+		return ResponseWrapper.from("hello-world!22");
 	}
 }
